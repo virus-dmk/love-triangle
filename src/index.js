142889139e96e429b6a45cbfd858869b81553fe3
Dmitry Bychko <virus_dmk@mail.ru>
2019-03-21 13:54:21 +0300

    feat/: done task

--- src/index.js
+++ src/index.js
@@ -3,5 +3,26 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let count = 0;
+  let mas = preferences;
+
+  for (let i = 0; i < mas.length; i++) {
+    
+    let firstPlace = i;
+    let second = mas[firstPlace]-1;
+    let third = mas[second]-1; 
+    let first = mas[third]-1; 
+    
+    
+    if(typeof first === 'undefined' 
+    || typeof second === 'undefined' 
+    || typeof third === 'undefined'  ) {
+      continue;
+    }
+    if( (first === firstPlace) && (second !== third) && (second!==first) && (third!==first) && first!==second && third !== second ){
+      count++;
+      
+    }
+  }
+  return count/3;
 };
